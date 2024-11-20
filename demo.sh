8d95125e36d80f68305fcb93bc367b114d35a6c3
Ashley Womack <awomack@Ashleys-MacBook-Air.local>
2024-11-20 12:57:39 -0600

    Adds script stuff

--- demo.sh
+++ demo.sh
@@ -1,2 +1,2 @@
-ab -n 60 -c 5 -s 69 "http://172.18.0.3/productpage"
+ab -n 60 -c 5 -s -v 9 "http://172.18.0.3/productpage"
 #for i in $(seq 1 100); do curl -s -o /dev/null "http://172.18.0.3/productpage"; done
